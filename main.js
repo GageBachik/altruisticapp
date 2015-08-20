613892c7e7ce51c6e04eed85efe3d0f965beee42
Gage Bachik <gbachik>
2015-08-19 18:56:16 -0700

    finished updates

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(function(){
-	// console.log('javascript executed')
 	var request = $.ajax({
-		url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5c26167b4a5960e56014a8fc89dd1390&tags=wealthy,altruistic,rich,luxury&safe_search=1&content_type=1&per_page=500&format=json&nojsoncallback=1",
+		url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=be87ef7d66e0c6e39f411d62a58f496c&tags=wealthy,altruistic,rich,luxury&safe_search=1&content_type=1&per_page=500&format=json&nojsoncallback=1",
 		method: "GET",
 		dataType: "JSON"
 	})
@@ -13,7 +12,7 @@ $(function(){
 
 		data.photos.photo.map(function (image) {
 			if (users.indexOf(image.owner) === -1){
-				$('.bg-collage').append('<img src="https://farm'+image.farm+'.staticflickr.com/'+image.server+'/'+image.id+'_'+image.secret+'_n.jpg" class="bg-image">')
+				$('#bg-collage').append('<div class="item"><img src="https://farm'+image.farm+'.staticflickr.com/'+image.server+'/'+image.id+'_'+image.secret+'_n.jpg" class="bg-image"></div>')
 				users.push(image.owner)
 				count++
 			}
@@ -22,15 +21,18 @@ $(function(){
 		// console.log("count:", count);
 		// console.log("users:", users);
 
-		$('.bg-collage').imagesLoaded( function() {
+		$('#bg-collage').imagesLoaded( function() {
 			// console.log('imagesloaded')
-			$('.bg-collage').masonry({
+			$('#bg-collage').masonry({
 				// options
-				itemSelector: '.bg-image',
-				columnWidth: 320,
-				isFitWidth: true
+				itemSelector: '.item',
+				columnWidth: '.grid-sizer'
 			});
 		});
+	});
+
+	request.error(function(error){
+		console.log("error:", error);
 	});
 
 })
